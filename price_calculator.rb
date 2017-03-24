0c2415f2db7ccf24eb5f2c74877a3896944a4bc9
Gianni Leggio <giannileggio@gmail.com>
2017-03-24 12:08:59 +0000

    Buying 2 different books

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,5 +1,6 @@
 class PriceCalculator
   def total(books)
+    return 15.2 if books.uniq.size == 2
     return 16 if books.size == 2
     8
   end
--- spec/price_calculator_spec.rb
+++ spec/price_calculator_spec.rb
@@ -2,7 +2,7 @@ require_relative '../price_calculator'
 
 RSpec.describe PriceCalculator do
   describe '#total' do
-    subject { PriceCalculator.new.total(books) }
+    subject { described_class.new.total(books) }
 
     context 'when buying 1 book' do
       let(:books) { [0] }
@@ -12,6 +12,14 @@ RSpec.describe PriceCalculator do
     context 'when buying 2 copies of same book' do
       let(:books) { [3, 3] }
       it { is_expected.to eq 16 }
+    end
+
+    context 'when buying 2 different books' do
+      let(:books) { [0, 1] }
+      it { is_expected.to eq 15.2 }
+    end
+
+    context 'when parameters is empty array' do
     end
   end
 end
